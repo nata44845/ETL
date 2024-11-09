5436d9cb13ec2f244b50baf76eea5f812e132f8d
nata44845 <krppost@yandex.ru>
2024-11-09 14:40:40 +0700

    hw2

--- seminar4/s4_2.py
+++ seminar4/s4_2.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from sqlalchemy import inspect, create_engine
 from pandas.io import sql
 import warnings, matplotlib
+from pyspark.sql.window import Window
+from pyspark.sql.functions import sum as sum1
 
 warnings.filterwarnings("ignore")
 t0 = time.time()
@@ -14,29 +16,28 @@ t0 = time.time()
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-con = create_engine("mysql://root:root@localhost/spark_4")
+con = create_engine("mysql://root:root@localhost/spark")
 
 spark = SparkSession.builder.appName("sem4").getOrCreate()
 
-if 1==2:
-        sql.execute("""drop table if exists spark_4.etl_4_2a""", con)
-        sql.execute("""CREATE TABLE if not exists spark_4.etl_4_2a (
-                num INT(10) NULL DEFAULT NULL,
-                mon DATE NULL DEFAULT NULL,
-                sum FLOAT NULL DEFAULT NULL,
-                debt_sum FLOAT NULL DEFAULT NULL,
-                persent_sum FLOAT NULL DEFAULT NULL,
-                debt_left FLOAT NULL DEFAULT NULL,
-                percent_summary FLOAT NULL DEFAULT NULL,
+w = Window.partitionBy(lit(1)).orderBy("num").rowsBetween(Window.unboundedPreceding, Window.currentRow)
+
+def create_table(table_name):
+        sql.execute(f"""drop table if exists {table_name}""", con)
+        sql.execute(f"""CREATE TABLE if not exists {table_name} (\
+                num INT(10) NULL DEFAULT NULL,\
+                mon DATE NULL DEFAULT NULL,\
+                sum FLOAT NULL DEFAULT NULL,\
+                debt_sum FLOAT NULL DEFAULT NULL,\
+                persent_sum FLOAT NULL DEFAULT NULL,\
+                debt_left FLOAT NULL DEFAULT NULL,\
+                percent_summary FLOAT NULL DEFAULT NULL,\
                 debt_summary FLOAT NULL DEFAULT NULL
-                )
-                COLLATE='utf8mb4_0900_ai_ci'
+                )\
                 ENGINE=InnoDB""", con)
 
-from pyspark.sql.window import Window
-from pyspark.sql.functions import sum as sum1
-w = Window.partitionBy(lit(1)).orderBy("num").rowsBetween(Window.unboundedPreceding, Window.currentRow)
-df1 = spark.read.format("com.crealytics.spark.excel")\
+def load_data_to_table(file_name, table_name):
+        df = spark.read.format("com.crealytics.spark.excel")\
         .option("sheetName", "Sheet1")\
         .option("useHeader", "false")\
         .option("treatEmptyValuesAsNulls", "false")\
@@ -49,131 +50,52 @@ df1 = spark.read.format("com.crealytics.spark.excel")\
         .option("excerptSize", 10)\
         .option("header", "true")\
         .format("excel")\
-        .load("d4_1.xlsx").limit(1000)\
+        .load(file_name).limit(1000)\
         .withColumn("percent_summary", sum1(col("percent_sum")).over(w))\
         .withColumn("debt_summary", sum1(col("debt_sum")).over(w))
 
-df1.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark_4?user=root&password=root\
+        df.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root\
                                 &sessionVariables=sql_mode='NO_ENGINE_SUBSTITUTION'&jdbcCompliantTruncation=false")\
-        .option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "etl_4_2a")\
+        .option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", table_name)\
         .mode("overwrite").save()
+        return df
+
+def add_plot(df, color1, color2):
+        df = df.toPandas()
+        # Get current axis 
+        ax = plt.gca()
+        ax.ticklabel_format(style='plain')
+        # bar plot
+        df.plot(kind='line', 
+                x='num', 
+                y='debt_summary', 
+                color=color1, ax=ax)
+        df.plot(kind='line', 
+                x='num', 
+                y='percent_summary', 
+                color=color2, ax=ax)
 
 
-df2 = df1.toPandas()
-# Get current axis 
-ax = plt.gca()
-ax.ticklabel_format(style='plain')
-# bar plot
-df2.plot(kind='line', 
-        x='num', 
-        y='debt_summary', 
-        color='green', ax=ax)
-df2.plot(kind='line', 
-        x='num', 
-        y='percent_summary', 
-        color='red', ax=ax)
+if 1==2:
+        create_table("spark.etl_4_2a")
+        create_table("spark.etl_4_2b")
+        create_table("spark.etl_4_2c")
+
+df1 = load_data_to_table("sem4_2_1.xlsx","etl_4_2a")
+df2 = load_data_to_table("sem4_2_2.xlsx","etl_4_2b")
+df3 = load_data_to_table("sem4_2_3.xlsx","etl_4_2c")
+
+df1.show(2)
+df2.show(2)
+df3.show(2)
+
+add_plot(df1, "green", "red")
+add_plot(df2, "blue", "cyan")
+add_plot(df3, "magenta", "yellow")
 
 # show the plot
 plt.legend(['долг_86689', 'проценты_86689','долг_120000', 'проценты_120000','долг_150000', 'проценты_150000'])
 plt.show()
-exit()
-
-sql.execute("""drop table if exists spark_4.etl_4_2b""",con)
-sql.execute("""CREATE TABLE if not exists spark_4.etl_4_2b (
-	`№` INT(10) NULL DEFAULT NULL,
-	`Месяц` DATE NULL DEFAULT NULL,
-	`Сумма платежа` FLOAT NULL DEFAULT NULL,
-	`Платеж по основному долгу` FLOAT NULL DEFAULT NULL,
-	`Платеж по процентам` FLOAT NULL DEFAULT NULL,
-	`Остаток долга` FLOAT NULL DEFAULT NULL,
-	`проценты` FLOAT NULL DEFAULT NULL,
-	`долг` FLOAT NULL DEFAULT NULL
-)
-COLLATE='utf8mb4_0900_ai_ci'
-ENGINE=InnoDB""",con)
-
-from pyspark.sql.window import Window
-from pyspark.sql.functions import sum as sum1
-w = Window.partitionBy(lit(1)).orderBy("№").rowsBetween(Window.unboundedPreceding, Window.currentRow)
-df3 = spark.read.format("com.crealytics.spark.excel")\
-        .option("sheetName", "Sheet1")\
-        .option("useHeader", "false")\
-        .option("treatEmptyValuesAsNulls", "false")\
-        .option("inferSchema", "true").option("addColorColumns", "true")\
-	.option("usePlainNumberFormat","true")\
-        .option("startColumn", 0)\
-        .option("endColumn", 99)\
-        .option("timestampFormat", "MM-dd-yyyy HH:mm:ss")\
-        .option("maxRowsInMemory", 20)\
-        .option("excerptSize", 10)\
-        .option("header", "true")\
-        .format("excel")\
-        .load("d4_2.xlsx").limit(1000)\
-        .withColumn("проценты", sum1(col("Платеж по процентам")).over(w))\
-        .withColumn("долг", sum1(col("Платеж по основному долгу")).over(w))
-df3.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root")\
-        .option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "etl_4_2b")\
-        .mode("append").save()
-df4 = df3.toPandas()
-ax.ticklabel_format(style='plain')
-# bar plot
-df4.plot(kind='line', 
-        x='№', 
-        y='долг', 
-        color='blue', ax=ax)
-df4.plot(kind='line', 
-        x='№', 
-        y='проценты', 
-        color='cyan', ax=ax)
-
-sql.execute("""drop table if exists spark.etl_4_2c""",con)
-sql.execute("""CREATE TABLE if not exists spark.d4_3 (
-	`№` INT(10) NULL DEFAULT NULL,
-	`Месяц` DATE NULL DEFAULT NULL,
-	`Сумма платежа` FLOAT NULL DEFAULT NULL,
-	`Платеж по основному долгу` FLOAT NULL DEFAULT NULL,
-	`Платеж по процентам` FLOAT NULL DEFAULT NULL,
-	`Остаток долга` FLOAT NULL DEFAULT NULL,
-	`проценты` FLOAT NULL DEFAULT NULL,
-	`долг` FLOAT NULL DEFAULT NULL
-)
-COLLATE='utf8mb4_0900_ai_ci'
-ENGINE=InnoDB""",con)
-from pyspark.sql.window import Window
-from pyspark.sql.functions import sum as sum1
-w = Window.partitionBy(lit(1)).orderBy("№").rowsBetween(Window.unboundedPreceding, Window.currentRow)
-df5 = spark.read.format("com.crealytics.spark.excel")\
-        .option("sheetName", "Sheet1")\
-        .option("useHeader", "true")\
-        .option("treatEmptyValuesAsNulls", "false")\
-        .option("inferSchema", "true").option("addColorColumns", "true")\
-	.option("usePlainNumberFormat","true")\
-        .option("startColumn", 0)\
-        .option("endColumn", 99)\
-        .option("timestampFormat", "MM-dd-yyyy HH:mm:ss")\
-        .option("maxRowsInMemory", 20)\
-        .option("excerptSize", 10)\
-        .option("header", "true")\
-        .format("excel")\
-        .load("d4_3.xlsx").limit(1000)\
-        .withColumn("проценты", sum1(col("Платеж по процентам")).over(w))\
-        .withColumn("долг", sum1(col("Платеж по основному долгу")).over(w))
-df5.write.format("jdbc").option("url","jdbc:mysql://localhost:3306/spark?user=root&password=root")\
-        .option("driver", "com.mysql.cj.jdbc.Driver").option("dbtable", "etl_4_2c")\
-        .mode("append").save()
-df6 = df5.toPandas()
-ax.ticklabel_format(style='plain')
-# bar plot
-df6.plot(kind='line', 
-        x='№', 
-        y='долг', 
-        color='magenta', ax=ax)
-df6.plot(kind='line', 
-        x='№', 
-        y='проценты', 
-        color='yellow', ax=ax)
-
-
 
 spark.stop()
 t1=time.time()
